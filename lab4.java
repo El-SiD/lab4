f44485159f24efd7f1abd971d11e5da094b86b22
Mikhail Tsvetkov <mikhail.tsvetkov.94@gmail.com>
2012-04-23 19:58:57 +0400

    initial commit

---
+++ lab4.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import javax.swing.*;
+import java.util.regex.*;
+
+	public class lab4
+	{
+		public static void main(String[] args) 
+		{
+			String file;
+			String text = "";
+			String tmp = "";
+			Matcher match;
+			Pattern patt;
+			if (args.length == 2 && args.length != 0)
+			{
+				file = args[0];
+				patt = Pattern.compile(args[1]);
+			} 
+			else
+			{
+				file = JOptionPane.showInputDialog("File: ");
+				patt = Pattern.compile(JOptionPane.showInputDialog("Regular expression: "));
+			}
+			try
+			{
+				FileInputStream file_in = new FileInputStream(file);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(file_in));
+				FileOutputStream file_out = new FileOutputStream("output.txt");
+				DataOutputStream writer = new DataOutputStream(file_out);
+				tmp = reader.readLine();
+				int i = 1;
+				do
+				{
+					match = patt.matcher(tmp);
+					if (match.matches())
+					{
+						text = text + "file_name:" + i + ": " + tmp + "\n";
+						//text += "\n";
+					}
+					tmp = reader.readLine();
+					i++;
+				}
+				while (tmp != null);
+				writer.writeUTF(text);
+				reader.close();
+				writer.close();
+			}
+			catch(Exception exptn)
+			{
+				/*if (tmp == "")
+					JOptionPane.showMessageDialog(null, "End of program");
+				else JOptionPane.showMessageDialog(null, tmp);*/
+			}
+		}
+	}
